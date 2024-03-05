06253318010ad49acb5fd9297784fd4b6d89ac42
Hassan Rahman <engr.hassanrahman@gmail.com>
2024-03-05 07:05:01 +0500

    AppWithImages added

---
+++ AppWithImages/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppWithImages
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppWithImages/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppWithImages
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
